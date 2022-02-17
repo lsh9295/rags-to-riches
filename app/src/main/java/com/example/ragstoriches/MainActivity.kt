89d96c805469bcaa2e2cfa7e2beb644f76c21dd8
lsh9295 <lsh929500@gmail.com>
2022-02-18 01:30:12 +0900

    QRcode and Review

--- app/src/main/java/com/example/ragstoriches/MainActivity.kt
+++ app/src/main/java/com/example/ragstoriches/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.ragstoriches
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,59 +9,27 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.Toast
+import com.example.ragstoriches.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding // 뷰 바인딩
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
 
-        val gen_num = findViewById<Button>(R.id.generate)
-        val save_num = findViewById<Button>(R.id.save)
-        val win_list = findViewById<Button>(R.id.winlist)
-        val qr_code = findViewById<Button>(R.id.qrcode)
-        val dvlp_review = findViewById<Button>(R.id.review)
-
-        qr_code.setOnClickListener{
-            val integrator = IntentIntegrator(this)
-            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE) // 여러가지 바코드중에 특정 바코드 설정 가능
-            integrator.setPrompt("QR 코드를 스캔하여 주세요 !") // 스캔할 때 하단의 문구
-            integrator.setCameraId(0) // 0은 후면 카메라, 1은 전면 카메라
-            integrator.setBeepEnabled(true) // 바코드를 인식했을 때 삑 소리유무
-            integrator.setBarcodeImageEnabled(false) // 스캔 했을 때 스캔한 이미지 사용여부
-            integrator.initiateScan() // 스캔
+        // QR코드 당첨확인
+        binding.qrcode.setOnClickListener {
+            val intent = Intent(this, QrActivity::class.java)
+            startActivity(intent)
         }
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        // QR 코드를 찍은 결과를 변수에 담는다.
-        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-        val web_view = findViewById<WebView>(R.id.webview)
-        Log.d("TTT", "QR 코드 체크")
-
-        //결과가 있으면
-        if (result != null) {
-            // 컨텐츠가 없으면
-            if (result.contents == null) {
-                //토스트를 띄운다.
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
-            }
-            // 컨텐츠가 있으면
-            else {
-                //토스트를 띄운다.
-                Toast.makeText(this, "scanned" + result.contents, Toast.LENGTH_LONG).show()
-                Log.d("TTT", "QR 코드 URL:${result.contents}")
-
-                //웹뷰 설정
-                web_view.settings.javaScriptEnabled = true
-                web_view.webViewClient = WebViewClient()
-
-                //웹뷰를 띄운다.
-                web_view.loadUrl(result.contents)
-            }
-            // 결과가 없으면
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
+        // 개발자 후기게시판
+        binding.review.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://seungheezz.tistory.com/category/%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C%20%EA%B0%9C%EB%B0%9C/%EC%9D%B8%EC%83%9D%EC%97%AD%EC%A0%84%20%EC%B6%94%EC%B2%A8%20%ED%9B%84%EA%B8%B0"))
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/ragstoriches/QrActivity.kt
+++ app/src/main/java/com/example/ragstoriches/QrActivity.kt
@@ -1,11 +1,63 @@
 package com.example.ragstoriches
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import com.example.ragstoriches.databinding.ActivityMainBinding
+import com.example.ragstoriches.databinding.ActivityQrBinding
+import com.google.zxing.integration.android.IntentIntegrator
 
 class QrActivity : AppCompatActivity() {
+    lateinit var binding : ActivityQrBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qr)
+        binding = ActivityQrBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
+        binding.btnClick.setOnClickListener{
+            val integrator = IntentIntegrator(this)
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE) // 여러가지 바코드중에 특정 바코드 설정 가능
+            integrator.setPrompt("QR 코드를 스캔하여 주세요 !") // 스캔할 때 하단의 문구
+            integrator.setCameraId(0) // 0은 후면 카메라, 1은 전면 카메라
+            integrator.setBeepEnabled(true) // 바코드를 인식했을 때 삑 소리유무
+            integrator.setBarcodeImageEnabled(false) // 스캔 했을 때 스캔한 이미지 사용여부
+            integrator.initiateScan() // 스캔
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        // QR 코드를 찍은 결과를 변수에 담는다.
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        val web_view = findViewById<WebView>(R.id.webview)
+        Log.d("TTT", "QR 코드 체크")
+
+        //결과가 있으면
+        if (result != null) {
+            // 컨텐츠가 없으면
+            if (result.contents == null) {
+                //토스트를 띄운다.
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            }
+            // 컨텐츠가 있으면
+            else {
+                //토스트를 띄운다.
+                Toast.makeText(this, "scanned" + result.contents, Toast.LENGTH_LONG).show()
+                Log.d("TTT", "QR 코드 URL:${result.contents}")
+
+                //웹뷰 설정
+                web_view.settings.javaScriptEnabled = true
+                web_view.webViewClient = WebViewClient()
+
+                //웹뷰를 띄운다.
+                web_view.loadUrl(result.contents)
+            }
+            // 결과가 없으면
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
